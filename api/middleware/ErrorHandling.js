d151aa36c3fca891be23e4d08ebf7a96dfaa66ba
Mihlali-Ma <129970639+Mihlali-Ma@users.noreply.github.com>
2023-08-17 14:30:13 +0200

    final touches

--- api/middleware/ErrorHandling.js
+++ api/middleware/ErrorHandling.js
@@ -1,1 +1,12 @@
-// error handling
+function errorHandling(err, req, res, next){
+    if(err){
+    let status = err.status || 500
+    res.json({
+        status,
+        msg:'an error has occurred; try again later'
+    })
+    }
+    next()
+}
+
+module.exports = errorHandling

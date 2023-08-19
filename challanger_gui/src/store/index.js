b407e49411a13f21d36b38f3306447b7c7507a18
Mihlali-Ma <129970639+Mihlali-Ma@users.noreply.github.com>
2023-08-19 14:22:42 +0200

    hey

--- challanger_gui/src/store/index.js
+++ challanger_gui/src/store/index.js
@@ -1,13 +1,52 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+
+const cUrl = ""
 
 export default createStore({
   state: {
+    Users:null,
+    User:null,
+    products:null,
+    product:null,
+    spinner:false,
+    token:null,
+    msg:null
   },
   getters: {
   },
   mutations: {
+    setUsers(state, Users){
+      state.Users = Users
+    },
+    setUser(state, User){
+      state.User = User
+    },
+    setProducts(state, products){
+      state.products = products
+    },
+    setProduct(state, Product){
+      state.product = Product
+    },
+    setSpinner(state, spinner){
+      state.spinner = spinner
+    },
+    setToken(state, toke){
+      state.token = toke 
+    },
+    setMsg(state, msg){
+      state.msg = msg
+    }
   },
   actions: {
+    async fetchUsers(context){
+      try{
+        const {results} = await axios.get(`${cUrl}users`)
+        context.commit("setUsers",data.results)
+      }catch(e){
+        context.commit("setMsg")
+      }
+    }
   },
   modules: {
   }
---
+++ challanger_gui/src/views/UsersView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h2></h2>
+        </div>
+        <div v-else class="row">
+            <spinner-comp/>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import spinnerComp from '../components/spinnerComp.vue'
+    export default {
+  components: { spinnerComp },
+        
+        computed:{
+            Users(){
+                return this.$store.state.Users
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
